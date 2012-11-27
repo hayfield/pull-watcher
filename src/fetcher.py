be7c18b8fb5a61ae1481f3407b36190f35cf0ca6
hayfield <hayfield@gmx.com>
2012-11-27 22:52:04 +0000

    Change the -u flag to -user

--- src/fetcher.py
+++ src/fetcher.py
@@ -6,12 +6,12 @@ import json
 
 def get_args():
 	parser = argparse.ArgumentParser(description='Create a fetcher.')
-	parser.add_argument('-u', type=str, nargs=1)
+	parser.add_argument('-user', type=str, nargs=1)
 	parser.add_argument('-repo', type=str, nargs=1)
 	parser.add_argument('-token', type=str, nargs=1)
 	args = parser.parse_args()
 
-	args.u = args.u[0]
+	args.user = args.user[0]
 	args.repo = args.repo[0]
 	print args
 
@@ -33,7 +33,7 @@ def url_base():
 	return 'https://api.github.com/'
 
 def fetch_repo():
-	fetch_url( url_base() + 'repos/' + get_args().u + '/' + get_args().repo )
+	fetch_url( url_base() + 'repos/' + get_args().user + '/' + get_args().repo )
 
 def main():
 	args = get_args()
