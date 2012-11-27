04b9ce38a2d2adcb3f0a871c27d19aad1aad0ae6
hayfield <hayfield@gmx.com>
2012-11-27 21:18:08 +0000

    Read the token from the elephant file if it's not specified

--- src/fetcher.py
+++ src/fetcher.py
@@ -1,17 +1,27 @@
 #! /usr/bin/env python
 
 import argparse
+import requests
 
-def main():
+def get_args():
 	parser = argparse.ArgumentParser(description='Create a fetcher.')
 	parser.add_argument('-u', type=str, nargs=1)
 	parser.add_argument('-repo', type=str, nargs=1)
+	parser.add_argument('-token', type=str, nargs=1)
 	args = parser.parse_args()
 
 	print args
-	print args.u[0]
-	print args.repo[0]
-	print('Hello World!')
+
+	if args.token == None:
+		f = open('../github-token.elephant', 'r')
+		args.token = f.readline()
+
+	return args
+
+def main():
+	args = get_args()
+
+	print args
 
 if __name__ == "__main__":
 	main()
