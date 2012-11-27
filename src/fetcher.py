7b8bcb21a8f2d9108dd8958945f9846d8de774cb
hayfield <hayfield@gmx.com>
2012-11-27 23:20:27 +0000

    Add stuff relating to dates and creating directories

--- src/fetcher.py
+++ src/fetcher.py
@@ -3,6 +3,8 @@
 import argparse
 import requests
 import json
+from datetime import datetime
+import os
 
 def get_args():
 	parser = argparse.ArgumentParser(description='Create a fetcher.')
@@ -28,12 +30,34 @@ def fetch_url(url):
 	if url.find('https://api.github.com') == 0:
 		r = requests.get(url + '?access_token=' + get_args().token)
 		print r.text
+		return r
 
 def url_base():
 	return 'https://api.github.com/'
 
 def fetch_repo():
-	fetch_url( url_base() + 'repos/' + get_args().user + '/' + get_args().repo )
+	r = fetch_url( url_base() + 'repos/' + get_args().user + '/' + get_args().repo )
+	data = json.loads(r.text)
+	updateDate = data['updated_at']
+	date = datetime.strptime('2012-11-27T16:22:34Z', '%Y-%m-%dT%H:%M:%SZ')
+	print date
+
+def data_dir():
+	return os.path.join('..', 'data')
+
+def repo_dir():
+	return os.path.join(data_dir(), get_args().user, get_args().repo)
+
+def setup_folders():
+	# the main data folder
+	dataDir = data_dir()
+	if not os.path.exists(dataDir):
+		os.makedirs(dataDir)
+
+	# the repo folder
+	repoDir = repo_dir()
+	if not os.path.exists(repoDir):
+		os.makedirs(repoDir)
 
 def main():
 	args = get_args()
@@ -43,6 +67,7 @@ def main():
 	fetch_url('henry')
 	fetch_url('https://api.github.com/rate_limit')
 	fetch_repo()
+	setup_folders()
 
 if __name__ == "__main__":
 	main()
