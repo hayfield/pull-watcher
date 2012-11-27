f5bb7e2c78f9785801038c00bccf39a891def596
hayfield <hayfield@gmx.com>
2012-11-27 21:30:59 +0000

    Add a functiont to fetch a URL

--- src/fetcher.py
+++ src/fetcher.py
@@ -2,6 +2,7 @@
 
 import argparse
 import requests
+import json
 
 def get_args():
 	parser = argparse.ArgumentParser(description='Create a fetcher.')
@@ -18,10 +19,19 @@ def get_args():
 
 	return args
 
+def fetch_url(url):
+	print url
+	if url.find('https://api.github.com') == 0:
+		r = requests.get(url + '?access_token=' + get_args().token)
+		print r.text
+
 def main():
 	args = get_args()
 
 	print args
+
+	fetch_url('henry')
+	fetch_url('https://api.github.com/rate_limit')
 
 if __name__ == "__main__":
 	main()
