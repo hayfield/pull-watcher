b40135d2d3f65429918e060b823487f9b4dcf782
hayfield <hayfield@gmx.com>
2012-11-27 21:04:58 +0000

    Add a couple of command line args to the fetcher to specify a user and repo

--- src/fetcher.py
+++ src/fetcher.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python
 
+import argparse
+
 def main():
+	parser = argparse.ArgumentParser(description='Create a fetcher.')
+	parser.add_argument('-u', type=str, nargs=1)
+	parser.add_argument('-repo', type=str, nargs=1)
+	args = parser.parse_args()
+
+	print args
+	print args.u[0]
+	print args.repo[0]
 	print('Hello World!')
 
 if __name__ == "__main__":
